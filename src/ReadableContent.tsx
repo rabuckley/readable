6f4f0f47960adc048840a7249ebb4aaba7df1934
Ryan Buckley <ryan@brownthwaite.com>
2025-04-06 15:14:01 +0100

    Warmer light-mode background

--- src/ReadableContent.tsx
+++ src/ReadableContent.tsx
@@ -15,8 +15,8 @@ const ReadableContent: React.FC<ReadableContentProps> = ({
   siteName,
 }) => {
   return (
-    <div className="grid grid-cols-[1fr_minmax(0,var(--container-3xl))_1fr] gap-4 dark:bg-neutral-900 dark:text-zinc-50">
-      <div className="prose prose-neutral dark:prose-invert md:prose-lg prose-blockquote:not-italic prose-headings:font-semibold prose-headings:font-sans prose-lead:text-neutral-900 dark:prose-lead:text-neutral-300 prose-pre:bg-neutral-200 prose-pre:text-neutral-800 dark:prose-pre:text-neutral-300 dark:prose-pre:bg-neutral-800 prose-code:font-medium prose-code:font-mono col-span-1 col-start-2 my-12 font-serif">
+    <div className="bg-oat-50 grid min-h-screen grid-cols-[1fr_minmax(0,var(--container-3xl))_1fr] gap-4 dark:bg-neutral-900">
+      <div className="prose prose-neutral dark:prose-invert md:prose-lg prose-blockquote:not-italic prose-headings:font-semibold prose-headings:font-sans prose-lead:text-neutral-900 dark:prose-lead:text-neutral-300 prose-pre:bg-neutral-100 prose-pre:text-neutral-800 dark:prose-pre:text-neutral-300 dark:prose-pre:bg-neutral-800 prose-code:font-medium prose-code:font-mono col-span-1 col-start-2 my-12 font-serif">
         <h1>{title}</h1>
         {(byline || siteName) && (
           <div className="mb-4 text-neutral-600 dark:text-neutral-300">
